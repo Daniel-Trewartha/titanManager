ef9e9de5c728889e8eb7a472ec6ed73c70be7105
Daniel Trewartha <danieltr@titan-ext2.ccs.ornl.gov>
2017-05-26 15:30:31 -0400

    walltime tracking

--- pbsManager.py
+++ pbsManager.py
@@ -1,21 +1,28 @@
 import os
 import sys
 import subprocess
-from base import totalNodes
+from base import totalNodes, Base, engine
+import DateTime
 
-def main(self):
+def main():
 	print "Commands for pbs"
-def getFreeResources(self):
-	cmd = "qstat -a"
+
+def getFreeResources():
+	cmd = "qstat -a | grep ' R '"
 	qstatOut = subprocess.Popen(cmd,stdout=subprocess.PIPE,shell=True).stdout.read()
 	freeNodes = totalNodes
-	for line in qstatOut:
-		print line
+	for line in str.split(qstatOut,'\n'):
+		sLine = str.split(line)
+		if (len(sLine) == 11):
+			if(int(sLine[5])):
+				freeNodes -= int(sLine[5])
+			print str.split(line)[8],str.split(line)[10]
+	print freeNodes
 
 if __name__ == '__main__':
     Base.metadata.create_all(engine)
     if(len(sys.argv)>1):
         if (sys.argv[1] == 'getFreeResources'):
-            print getFreeResources
+            print getFreeResources()
     else:
         main()
