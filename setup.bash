92cbb199e5d3156504ecec223468cd67a4187b46
Daniel Trewartha <danieltr@titan-ext6.ccs.ornl.gov>
2017-06-27 15:08:08 -0400

    Added setup script and requirements.txt

---
+++ setup.bash
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+module load python
+module load python_setuptools
+module load python_virtualenv
+virtualenv titanManager
+source titanManager/bin/activate
+pip install -r requirements.txt
+#pip standard repos do not find faker for some reason
+easy_install faker
